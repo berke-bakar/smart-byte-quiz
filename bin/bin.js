d99c90a1d8d8c7d0dc47e809dc88e5318fc1bbf8
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-07-17 23:19:52 +0300

    -> Implemented game loop logic

--- bin/bin.js
+++ bin/bin.js
@@ -1,6 +1,58 @@
 #!/usr/bin/env node
-function main() {
+import ConfigInstance from "../src/ConfigStorage.js"
+import { GameStates, showAgainPage, showCreditsPage, showMenuPage, showPlayPage, showSettingsPage, showTitlePage, fetchQuestions } from "../src/index.js";
+
+
+async function main() {
+  let state = GameStates.TITLE;
+  let results = {
+    correct: 0,
+    wrong: 0
+  }
+
+  gameLoop: while (state !== GameStates.QUIT) {
+    switch (state) {
+      case GameStates.TITLE:
+        showTitlePage()
+        state = GameStates.MENU
+        break
+      case GameStates.MENU:
+        const selection = await showMenuPage()
+        state = GameStates[selection]
+        break
+      case GameStates.SETTINGS:
+        const changes = await showSettingsPage()
+        if (changes) {
+          ConfigInstance.setBatch(changes)
+        }
+        state = GameStates.MENU
+        break
+      case GameStates.PLAY:
+        // Reset game score
+        results.correct = 0
+        results.wrong = 0
+        const questions = await fetchQuestions(ConfigInstance.get('difficulty'), ConfigInstance.get('limit'))
+        if (questions.length !== 0) {
+          results = await showPlayPage(questions)
+          state = GameStates.AGAIN
+        } else {
+          state = GameStates.MENU
+        }
+        break
+      case GameStates.AGAIN:
+        const userChoice = await showAgainPage(results)
+        state = GameStates[userChoice]
+        break
+      case GameStates.CREDITS:
+        await showCreditsPage()
+        state = GameStates.MENU
+        break
+      default:
+        // State is QUIT, but just in case
+        break gameLoop
+    }
+  }
 
 }
 
-main()
+await main()
